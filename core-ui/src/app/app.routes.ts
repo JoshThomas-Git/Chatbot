37b6ac70d3f04844960ed79b814776c94dc2f027
Josh.T <josh.t@lifetrenz.com>
2025-12-02 12:52:17 +0530

    Java Initial Setup

--- core-ui/src/app/app.routes.ts
+++ core-ui/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Mainpage } from './modules/mainpage/mainpage';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Mainpage }
+];
